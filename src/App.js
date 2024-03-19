949ac81a01302ad474a0c64e543727279690351a
Zaynab Mourtada <zaynabmo@umich.edu>
2024-03-18 23:50:09 -0400

    Add category implemented

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import './App.css';
 import Home from './JedwalComponents/Home';
 import Body from './JedwalComponents/Body';
 import InputOverlay from './JedwalComponents/NewTaskButton';
+import CategoryButton from './JedwalComponents/CategoryButton';
+
 
 function App() {
   const [showOverlay, setShowOverlay] = useState(false);
   // State to hold the selected dropdown option
   const [selectedOption, setSelectedOption] = useState('');
 
+  // State to hold the list of categories
+  const [categories, setCategories] = useState([]);
+
   const handleDropdownChange = (e) => {
     setSelectedOption(e.target.value);
+  };
+
+  const handleAddCategory = (newCategory) => {
+    setCategories((prevCategories) => [...prevCategories, newCategory]);
   };
 
   return (
     <div className="App">
 
       <Home />
+      <CategoryButton onAddCategory={handleAddCategory} />
+      {categories.map((category, index) => (
+      <div key={index}>{category}</div>
+      ))}
     </div>
   );
 }
