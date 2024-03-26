aab0c1c4d5315a4b6d7d6076b4174f1bc06c3e79
MuhannadAljabri <97546261+MuhannadAljabri@users.noreply.github.com>
2024-03-26 16:14:26 -0400

    adding the Add Task button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import { useState } from 'react';
 import './App.css';
 import Home from './JedwalComponents/Home';
-import PlusButton from './JedwalComponents/NewTaskButton';
-import Body from './JedwalComponents/Body';
-import InputOverlay from './JedwalComponents/NewTaskButton';
 import CategoryButton from './JedwalComponents/CategoryButton';
-
+import './NewTask.css';
+import AddTaskButton from './JedwalComponents/NewTaskButton'
 
 function App() {
 
   // State to hold the list of categories
   const [categories, setCategories] = useState([]);
-
-  const handleDropdownChange = (e) => {
-    setSelectedOption(e.target.value);
-  };
+  const [tasks, setTasks] = useState([]);
 
   const handleAddCategory = (newCategory) => {
     setCategories((prevCategories) => [...prevCategories, newCategory]);
+  };
+  const handleAddTask = (taskTitle, priority, description) => {
+    setTasks([...tasks, { title: taskTitle, priority, description }]);
   };
 
   return (
     <div className="App">
 
       <Home />
+      <div className='top-row'>
       <CategoryButton onAddCategory={handleAddCategory} />
-      {categories.map((category, index) => (
+
+            {categories.map((category, index) => (
       <div key={index}>{category}</div>
       ))}
+      <div><AddTaskButton onAddTask={handleAddTask} /></div>
+
+      </div>
+
     </div>
   );
 }
