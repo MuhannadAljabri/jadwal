aef21f11caa3f4c571d710941151b93c6bc9faa4
Zaynab Mourtada <zaynabmo@umich.edu>
2024-03-27 21:00:10 -0400

    Task Layout implemented

---
+++ src/JedwalComponents/TaskLayout.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function TaskLayout() {
+  return (
+    <div className="layouts-container">
+    <div className="layout-card1">
+      <h1>Today</h1>
+      <div className = "line"></div>
+    </div>
+    <div className = "layout-card2">
+      <h1> Tomorrow</h1>
+      <div className = "line"></div>
+    </div>
+    </div>
+  );
+}
+
+export default TaskLayout;
